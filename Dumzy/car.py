7bb05b8a2f440fc56eda8ee05904fe67d65acf5d
theVitalS <vit.shar.jr@gmail.com>
2025-01-05 21:52:12 +0100

    dumzy v0.1 final version

--- Dumzy/car.py
+++ Dumzy/car.py
@@ -3,83 +3,52 @@ import time
 import random
 
 # Define the GPIO pins for motor control
-lf = Pin(13, Pin.OUT)
-lb = Pin(12, Pin.OUT)
-rf = Pin(11, Pin.OUT)
-rb = Pin(10, Pin.OUT)
+rf = Pin(4, Pin.OUT)
+rb = Pin(5, Pin.OUT)
+lf = Pin(6, Pin.OUT)
+lb = Pin(7, Pin.OUT)
 
-'''
-ENA = PWM(Pin(17))
-ENB = PWM(Pin(16))# PWM pin for speed control (ENA)
+ENA = PWM(Pin(3))
+ENB = PWM(Pin(8))  # PWM pin for speed control (ENA)
 
 # Set PWM Frequency
-ENA.freq(1000)          # Set frequency to 1kHz
-ENB.freq(1000)  
+ENA.freq(1000)  # Set frequency to 1kHz
+ENB.freq(1000)
 
-#standard_speed = 50
-speed = 20
+# standard_speed = 50
+drag_factor = 1.25
+speed = 45
 ENA.duty_u16(int(speed * 65535 / 100))
-ENB.duty_u16(int(speed * 65535 / 100))
-
-
-
-def both_speed(sp=speed):
-    ENA.duty_u16(int(sp * 65535 / 100))
-    ENB.duty_u16(int(sp * 65535 / 100))
-    
-def left_speed(sp=speed):
-    ENB.duty_u16(int(sp * 65535 / 100))
-
-def both_speed(sp=speed):
-    ENA.duty_u16(int(sp * 65535 / 100))
-    
-
-def slight_right(sp=speed, t=1):
-    right_seed(0.7*sp)   
-    time.sleep(t)
-    right_seed(sp)
-    
-    
-def slight_left(sp=speed, t=1):
-    left_seed(0.7*sp)   
-    time.sleep(t)
-    left_seed(sp)  
-'''
-
-
+ENB.duty_u16(int(speed * drag_factor * 65535 / 100))
 
 
 def left_forward():
-    #print('left_forward')
+    # print('left_forward')
     lb.low()
     lf.high()
-    
-    
+
+
 def right_forward():
-    #print('right_forward')
+    # print('right_forward')
     rb.low()
     rf.high()
-    
-    
+
+
 def left_backward():
-    #print('left_backward')
+    # print('left_backward')
     lf.low()
     lb.high()
-    
-    
+
+
 def right_backward():
-    #print('right_backward')
+    # print('right_backward')
     rf.low()
     rb.high()
-    
 
 
 def move_forward():
     left_forward()
     right_forward()
-
-
-
 
 
 def move_backward():
@@ -92,21 +61,22 @@ def turn_left(n=1.5):
     right_forward()
     time.sleep(n)
     stop()
-    
-    
+
+
 def turn_right(n=1.5):
     left_forward()
     right_backward()
     time.sleep(n)
     stop()
 
+
 def stop():
     lf.low()
     lb.low()
     rf.low()
     rb.low()
-    
-    
+
+
 def random_turn():
     rot = random.uniform(0.3, 2)
     dir = random.randint(1, 2)
@@ -114,6 +84,7 @@ def random_turn():
         turn_right(rot)
     else:
         turn_left(rot)
+
 
 def test1():
     stop()
@@ -136,8 +107,8 @@ def test1():
     move_backward()
     time.sleep(1)
     stop()
-    
-    
+
+
 def test2():
     stop()
     time.sleep(0.5)
@@ -152,23 +123,22 @@ def test2():
     time.sleep(1)
     turn_right()
     stop()
-    
-    
-   
-def test3 (x):
+
+
+def test3(x):
     i = 0
     while i < x:
-
         move_forward()
         time.sleep(1)
         stop()
         time.sleep(2)
-        
+
         # Example: Turn left for 1 second, then stop
         move_backward()
         time.sleep(1)
         stop()
         time.sleep(1)
         i += 1
+
 
 
--- Dumzy/main.py
+++ Dumzy/main.py
@@ -3,72 +3,54 @@ import random
 import time
 from car import *
 from HC_SR04_distance_sensor import *
-    
-
-ENA = PWM(Pin(17))
-ENB = PWM(Pin(16))# PWM pin for speed control (ENA)
-
-# Set PWM Frequency
-ENA.freq(1000)          # Set frequency to 1kHz
-ENB.freq(1000)  
-
-#standard_speed = 50
-speed = 70
-ENA.duty_u16(int(speed * 65535 / 100))
-ENB.duty_u16(int(speed * 65535 / 100)) 
-
 
 stop()
 time.sleep(2)
-    
 
-def main(n = 40, random_stops=False):
+
+def main(n=40, random_stops=False):
     reps = 0
     while reps < n:
-        
-        distance = get_distance()       
+
+        distance = get_distance()
         if not distance:
             print("Distance measurement failed. Stopping robot.")
             stop()
             return
         print("Distance: {:.2f} cm".format(distance))
-        
+
         start_time = time.time()
-        
+
         while distance > 30:
             move_forward()
             time.sleep(0.01)
-            
+
             distance = get_distance(500)
             print("Distance: {:.2f} cm".format(distance))
 
-            
             if random_stops:
                 current_time = time.time()
-                t = current_time-start_time
+                t = current_time - start_time
                 if t <= random.randint(3, 6):
                     print('Go random turn')
                     random_turn()
-                    
-            
-            
-            
+
         stop()
         time.sleep(1)
-        
+
         move_backward()
         time.sleep(0.6)
-        
+
         stop()
         time.sleep(1)
-        
+
         random_turn()
-                  
+
         reps += 1
-        
+
 
 def disco():
-    freq = [2,3]
+    freq = [2, 3]
     while True:
         for f in freq:
             turn_right(f)
@@ -77,15 +59,16 @@ def disco():
             turn_left(f)
             stop()
             time.sleep(0.3)
-    
+
 
 main()
-#hc_sr04_example_usage()   
-    
-    
-    
-    
-    
+# hc_sr04_example_usage()
+
+
+
+
+
+
 
 
 
