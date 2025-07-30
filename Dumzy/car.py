7aec783b1ddea71a0b87a79ba07255dd0b013a3d
theVitalS <vit.shar.jr@gmail.com>
2025-07-30 17:16:34 +0200

    car.py deleted legacy code

--- Dumzy/car.py
+++ Dumzy/car.py
@@ -1,6 +1,4 @@
 from machine import Pin, PWM
-import time
-import random
 
 # Define the GPIO pins for motor control
 rf = Pin(4, Pin.OUT)
@@ -24,11 +22,6 @@ turning_slowdown = 2
 def set_speed(speed, drag_factor=drag_factor, turn_left_factor=0):
     ENA.duty_u16(int(speed * (1 + drag_factor + turn_left_factor) * 65535 / 100))
     ENB.duty_u16(int(speed * (1 - drag_factor - turn_left_factor) * 65535 / 100))
-
-
-def set_speeds(speed_a, speed_b):
-    ENA.duty_u16(int(speed_a * 65535 / 100))
-    ENB.duty_u16(int(speed_b * 65535 / 100))
 
 
 def set_right_speed(speed, to_print=True):
@@ -63,30 +56,26 @@ set_speed(base_speed)
 
 
 def left_forward():
-    # print('left_forward')
     lb.low()
     lf.high()
 
 
 def right_forward():
-    # print('right_forward')
     rb.low()
     rf.high()
 
 
 def left_backward():
-    # print('left_backward')
     lf.low()
     lb.high()
 
 
 def right_backward():
-    # print('right_backward')
     rf.low()
     rb.high()
 
 
-def move_forward(to_print=True):
+def move_forward(to_print=False):
     if to_print:
         print('moving_forward')
     left_forward()
@@ -98,91 +87,17 @@ def move_backward():
     right_backward()
 
 
-def turn_left(n=1.5):
-    # set_speed(base_speed/turning_slowdown, drag_factor=0)
+def turn_left():
     left_backward()
     right_forward()
-    # time.sleep(n)
-    # stop()
 
-
-def turn_right(n=1.5):
-    # set_speed(base_speed/turning_slowdown, drag_factor=0)
+def turn_right():
     left_forward()
     right_backward()
-    # time.sleep(n)
-    # stop()
-
 
 def stop():
     lf.low()
     lb.low()
     rf.low()
     rb.low()
-
-
-def random_turn():
-    rot = random.uniform(0.3, 2)
-    dir = random.randint(1, 2)
-    if dir == 1:
-        turn_right(rot)
-    else:
-        turn_left(rot)
-
-
-def test1():
-    stop()
-    time.sleep(0.5)
-    move_forward()
-    time.sleep(1)
-    stop()
-    time.sleep(1)
-    move_backward()
-    time.sleep(1)
-    stop()
-    turn_left()
-    time.sleep(1)
-    turn_right()
-    time.sleep(1)
-    move_forward()
-    time.sleep(1)
-    stop()
-    time.sleep(1)
-    move_backward()
-    time.sleep(1)
-    stop()
-
-
-def test2():
-    stop()
-    time.sleep(0.5)
-    move_forward()
-    time.sleep(1)
-    stop()
-    time.sleep(1)
-    move_backward()
-    time.sleep(1)
-    stop()
-    turn_left()
-    time.sleep(1)
-    turn_right()
-    stop()
-
-
-def test3(x):
-    i = 0
-    while i < x:
-        move_forward()
-        time.sleep(1)
-        stop()
-        time.sleep(2)
-
-        # Example: Turn left for 1 second, then stop
-        move_backward()
-        time.sleep(1)
-        stop()
-        time.sleep(1)
-        i += 1
-
-
 
